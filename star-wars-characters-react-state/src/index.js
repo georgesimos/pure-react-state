b32bdcb164188c4a7892781baef73878d54a5c3a
George <admin@georgesimos.com>
2020-02-23 13:42:27 +0200

    part 2 useFetch using async await

--- star-wars-characters-react-state/src/index.js
+++ star-wars-characters-react-state/src/index.js
@@ -18,16 +18,19 @@ const useFetch = url => {
     setResponse(null);
     setError(null);
 
-    fetch(url)
-      .then(response => response.json())
-      .then(response => {
-        setLoading(false);
-        setResponse(response);
-      })
-      .catch(error => {
-        setLoading(false);
+    const fetchUrl = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setResponse(data);
+      } catch (error) {
         setError(error);
-      });
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUrl();
   }, []);
 
   return [response, loading, error];
