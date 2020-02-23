53732014ea357aa14fd92f9e50408198ed588909
George <admin@georgesimos.com>
2020-02-23 18:45:33 +0200

    useFetch with fetchReducer

--- star-wars-characters-react-state/src/index.js
+++ star-wars-characters-react-state/src/index.js
@@ -8,30 +8,55 @@ import CharacterList from './CharacterList';
 import endpoint from './endpoint';
 import './styles.scss';
 
+const initialState = {
+  result: null,
+  loading: true,
+  error: null,
+};
+
+const fetchReducer = (state, action) => {
+  if (action.type === 'LOADING') {
+    return {
+      result: null,
+      loading: true,
+      error: null,
+    };
+  }
+  if (action.type === 'RESPONSE_COMPLETE') {
+    return {
+      result: action.payload.response,
+      loading: false,
+      error: null,
+    };
+  }
+  if (action.type === 'ERROR') {
+    return {
+      result: null,
+      loading: true,
+      error: action.payload.error,
+    };
+  }
+  return state;
+};
+
 const useFetch = url => {
-  const [response, setResponse] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [state, dispatch] = React.useReducer(fetchReducer, initialState);
 
   useEffect(() => {
-    setLoading(true);
-    setResponse(null);
-    setError(null);
+    dispatch({ type: 'LOADING' });
 
     (async function fetchUrl() {
       try {
         const response = await fetch(url);
         const data = await response.json();
-        setResponse(data);
+        dispatch({ type: 'RESPONSE_COMPLETE', payload: { response: data } });
       } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
+        dispatch({ type: 'ERROR', payload: { error } });
       }
     })();
   }, []);
 
-  return [response, loading, error];
+  return [state.result, state.loading, state.error];
 };
 
 const Application = () => {
