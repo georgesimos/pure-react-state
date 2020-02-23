3cadfb320d5b23ff8b3932c2e41b7eaa4c95d33f
George <admin@georgesimos.com>
2020-02-23 20:26:39 +0200

    Custom setState Hook

---
+++ user-signup-react-state/src/useSetState.js
@@ -0,0 +1,15 @@
+import { useReducer } from "react";
+
+const reducer = (previousState = {}, updateState = {}) => {
+  return { ...previousState, ...updateState };
+};
+
+const useSetState = (initialState = {}) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const setState = updateState => dispatch(updateState);
+
+  return [state, setState];
+};
+
+export default useSetState;
