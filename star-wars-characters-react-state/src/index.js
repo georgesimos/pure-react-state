23954262d68eda8e950fb7192f3270b266ae76da
George <admin@georgesimos.com>
2020-02-23 13:30:10 +0200

    Response, Loading & error part 1

--- star-wars-characters-react-state/src/index.js
+++ star-wars-characters-react-state/src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -7,10 +7,30 @@ import CharacterList from './CharacterList';
 
 import dummyData from './dummy-data';
 
+import endpoint from './endpoint';
 import './styles.scss';
 
 const Application = () => {
-  const [characters, setCharacters] = useState(dummyData);
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    setCharacters([]);
+    setError(null);
+
+    fetch(endpoint + '/characters')
+      .then(response => response.json())
+      .then(({ characters }) => {
+        setLoading(false);
+        setCharacters(characters);
+      })
+      .catch(error => {
+        setLoading(false);
+        setError(error);
+      });
+  }, []);
 
   return (
     <div className="Application">
@@ -19,7 +39,8 @@ const Application = () => {
       </header>
       <main>
         <section className="sidebar">
-          <CharacterList characters={characters} />
+          {loading ? <p>Loading</p> : <CharacterList characters={characters} />}
+          {error && <p className="error"> {error.message}</p>}
         </section>
       </main>
     </div>
